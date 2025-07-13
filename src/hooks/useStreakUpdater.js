d130d5ee15f0bfad2f123ab9c54d4113db5c21c1
Aditya Chaudhary <chaudharyaadityaa14@gmail.com>
2025-07-13 12:36:59 +0530

    Optimization

--- src/hooks/useStreakUpdater.js
+++ src/hooks/useStreakUpdater.js
@@ -1,7 +1,7 @@
 // src/hooks/useStreakUpdater.js
 import { useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
-import updateStreak from '../utils/updateStreak';
+import {updateStreak} from '../utils/updateStreak';
 
 export default function useStreakUpdater() {
   const { user } = useAuth();
